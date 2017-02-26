4e61a90476db1cbe0a06a34a0d026784f0ecac67
beckerdo <dan@danbecker.info>
2017-02-26 07:43:35 -0600

    Basic function created

--- src/main/java/info/danbecker/wordgenerator/WordGenerator.java
+++ src/main/java/info/danbecker/wordgenerator/WordGenerator.java
@@ -7,7 +7,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
+import java.util.regex.MatchResult;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -32,32 +34,56 @@ public class WordGenerator {
 	
 	public static Map<WordType,List<Word>> wordLists = new HashMap<>();
 	public static String[] wordSources = null; 
+	
+	public static Random random = new Random();
 
 	public static void main(String[] args) throws Exception {
 		LOGGER.debug("Hello");
 		System.out.println("WordGenerator");
 		// Parse command line options
 		parseGatherOptions(args);
-		System.out.println("numPatterns=" + numPatterns);
-
-		// Init word lists
-		for ( WordType wordType : WordType.values()) {
-			wordLists.put( wordType, new ArrayList<Word>());
-		}
-		
-		if (null != wordSources) {
-			int wordCount = 0;
-			for (int i = 0; i < wordSources.length; i++) {
-				System.out.println("word source " + i + "=" + wordSources[i]);
-				wordCount += scanText(wordLists, wordSources[i]);
+		if (null != pattern) {
+			WordType[] patternType = new WordType[pattern.length()];
+			for (int i = 0; i < pattern.length(); i++) {
+				patternType[i] = WordType.fromAbbreviation(pattern.substring(i, i + 1));
 			}
-			System.out.println("Word count=" + wordCount);
-		}
-		
-		for ( WordType wordType : WordType.values()) {
-			System.out.println( wordType.getAbbreviation() + " count=" + wordLists.get( wordType ).size() );
-		}
-				
+
+			// Init word lists
+			for (WordType wordType : WordType.values()) {
+				wordLists.put(wordType, new ArrayList<Word>());
+			}
+
+			if (null != wordSources) {
+				int wordCount = 0;
+				for (int i = 0; i < wordSources.length; i++) {
+					System.out.println("word source " + i + "=" + wordSources[i]);
+					wordCount += scanText(wordLists, wordSources[i]);
+				}
+				System.out.println("   word count=" + wordCount);
+			}
+
+			for (WordType wordType : WordType.values()) {
+				System.out.println("   " + wordType.getAbbreviation() + " count=" + wordLists.get(wordType).size());
+			}
+
+			for (int phrasei = 0; phrasei < numPatterns; phrasei++) {
+				StringBuffer phrase = new StringBuffer();
+				for (int wordi = 0; wordi < patternType.length; wordi++) {
+					if ( wordi > 0 ) {
+						phrase.append(" " );
+					}
+					List<Word> words = wordLists.get( patternType[wordi] );
+					if ( null != words && words.size() > 0 ) {						
+						// System.out.println( "word " + wordi + ",type=" +  patternType[wordi] + ", size=" + words.size() );
+						Word word = words.get( random.nextInt( words.size()) );
+						if ( null != word && null != word.text ) {
+							phrase.append( word.text );
+						}
+					}
+				}
+				System.out.println( phrase.toString() ); // the phrase!				
+			}
+		} // non-null pattern
 		LOGGER.debug("Bye");
 	}
 
@@ -81,13 +107,13 @@ public class WordGenerator {
 					options);
 			System.exit(0);
 		}
-		if (line.hasOption("numWords")) {
-			numPatterns = Integer.parseInt(line.getOptionValue("numWords"));
-			System.out.println("numwords=" + numPatterns);
+		if (line.hasOption("numPatterns")) {
+			numPatterns = Integer.parseInt(line.getOptionValue("numPatterns"));
+			System.out.println("   numPatterns=" + numPatterns);
 		}
 		if (line.hasOption("pattern")) {
 			pattern = line.getOptionValue("pattern");
-			System.out.println("pattern=" + pattern);
+			System.out.println("   pattern=" + pattern);
 		}
 
 		wordSources = line.getArgs(); // Prints non options.
@@ -110,7 +136,7 @@ public class WordGenerator {
 			String line = scanner.next();
             Word word = parseWordLine(line);
             if ( null != word ) {
-            	System.out.println( "word " + wordCount + "=" + word.text);
+            	// System.out.println( "word " + wordCount + "=" + word.text);
             	WordType type = word.type;            	
             	wordLists.get(type).add(word);
             	wordCount++;
@@ -151,22 +177,28 @@ public class WordGenerator {
         (.*)    Capture all
         (ab|cd) Matches ab or cd
         */        
-        scanner.useDelimiter("\\s*\\(\\)-\\s*");
-        String text = null;
-        if ( scanner.hasNext() ) {
-           text = scanner.next();
+        // scanner.useDelimiter("\\s*\\(\\)-\\s*");
+        scanner.findInLine("\\s*(#)\\s*(.*)|\\s*(\\w+)\\s*\\((\\w+)\\)\\s*-\\s*(.*)");
+        MatchResult result = scanner.match();
+        int groupCount = result.groupCount();
+        // if ( groupCount > 0 ) {
+        //     System.out.println( "input=" + result.group(0));        	
+        // }
+        if( groupCount > 1 && "#".equals( result.group ( 1  ))) {
+            // System.out.println( "comment=" + result.group(2));
+            scanner.close();
+            return null;
         }
+        String text = result.group( 3 );
         if ( null == text || text.startsWith( "#" )) {
         	scanner.close();
         	return null;
         }
-        // WordType type = scanner.next();
-        String desc = null;
-        if ( scanner.hasNext() ) {
-           desc = scanner.next();
-        }
+        String typeStr = result.group( 4 );
+        WordType type = WordType.fromAbbreviation( typeStr.toLowerCase() );
+        String desc = result.group( 5 );
         scanner.close();
-       	return new Word( text, WordType.NOUN, desc);
+       	return new Word( text, type , desc);
    }
 
 }
--- src/main/java/info/danbecker/wordgenerator/WordType.java
+++ src/main/java/info/danbecker/wordgenerator/WordType.java
@@ -5,7 +5,8 @@ public enum WordType {
 	VERB( "v", "v" ),
 	ADJECTIVE( "j", "adj" ),
 	ADVERB( "e", "adv" ),
-	PRONOUN( "p", "pro" );
+	PRONOUN( "p", "pro" ),
+	OTHER( "o", "oth");
 
 	String shortcut;
 	String abbreviation;
@@ -19,5 +20,23 @@ public enum WordType {
 	}
 	public String getAbbreviation() {
 		return abbreviation; 
+	}
+	
+	/** Returns an enum based on the given shortcut or abbreviation. */
+	public static WordType fromAbbreviation( String typeStr ) {
+        WordType type = WordType.OTHER;
+        if ( null != typeStr ) {
+            switch ( typeStr.toLowerCase() ) {
+        		case "n": type = WordType.NOUN; break;
+        		case "v": type = WordType.VERB; break;
+        		case "adj": type = WordType.ADJECTIVE; break;
+        		case "j": type = WordType.ADJECTIVE; break;
+        		case "adv": type = WordType.ADVERB; break;
+        		case "e": type = WordType.ADVERB; break;
+        		case "pro": type = WordType.PRONOUN; break;
+        		case "p": type = WordType.PRONOUN; break;
+            }        	
+        }
+        return type;
 	}
 }
